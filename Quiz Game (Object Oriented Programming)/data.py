70f49bca502e558469f438c87a7e0dd1ecf1259d
Tugberk Karan <118132230+tugberkkaran@users.noreply.github.com>
2023-06-02 15:06:27 +0300

    Create data.py

---
+++ Quiz Game (Object Oriented Programming)/data.py
@@ -0,0 +1,347 @@
+questions = [
+    {
+        "category": "History",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "The first televised presidential debate was between Jimmy Carter and Gerald Ford.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+    {
+        "category": "History",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "The M41 Walker Bulldog remains in service in some countries to this day.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "category": "History",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "Former United States Presidents John Adams and Thomas Jefferson died within hours of each other.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "category": "History",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "Theodore Roosevelt Jr. was the only General involved in the initial assault on D-Day.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "category": "History",
+        "type": "boolean", "difficulty": "easy",
+        "question": "The United States of America declared their independence from the British Empire on July 4th, 1776.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "category": "History",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "United States President Ronald Reagan was the first president to appoint a woman to the Supreme Court. ",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "category": "History",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "The Hundred Years&#039; War was fought for more than a hundred years.", "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "category": "History",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "The United States of America was the first country to launch a man into space.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+    {
+        "category": "History",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "Sir Issac Newton served as a Member of Parliament, but the only recorded time he spoke was to complain about a draft in the chambers.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "category": "History",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "Franz Joseph I was the last emperor of Austria.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+    {
+        "category": "Sports",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "Skateboarding will be included in the 2020 Summer Olympics in Tokyo.", "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "category": "Sports",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "Tennis was once known as Racquetball.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+    {
+        "category": "Sports",
+        "type": "boolean", "difficulty": "easy",
+        "question": "Roger Federer is a famous soccer player.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+    {
+        "category": "Sports", "type": "boolean",
+        "difficulty": "medium",
+        "question": "Formula E is an auto racing series that uses hybrid electric race cars.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+    {
+        "category": "Sports", "type": "boolean",
+        "difficulty": "easy",
+        "question": "In association football, or soccer, a corner kick is when the game restarts after someone scores a goal.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+    {
+        "category": "Sports", "type": "boolean",
+        "difficulty": "easy",
+        "question": "In Rugby League, performing a &quot;40-20&quot; is punished by a free kick for the opposing team.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+    {
+        "category": "Sports", "type": "boolean",
+        "difficulty": "medium",
+        "question": "Wilt Chamberlain scored his infamous 100-point-game against the New York Knicks in 1962.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "category": "Sports", "type": "boolean",
+        "difficulty": "medium",
+        "question": "During Wimbledon, spectators in the grounds can buy the tennis balls that have been used in matches.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "category": "Sports", "type": "boolean",
+        "difficulty": "medium",
+        "question": "In 2008, Usain Bolt set the world record for the 100 meters with one shoelace untied.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "category": "Sports",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "There are a total of 20 races in Formula One 2016 season.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+    {
+        "category": "Animals",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "A slug&rsquo;s blood is green.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "category": "Animals", "type": "boolean",
+        "difficulty": "easy",
+        "question": "Kangaroos keep food in their pouches next to their children.", "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+    {
+        "category": "Animals",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "A bear does NOT defecate during hibernation. ",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "category": "Animals",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "The average lifespan of a wildcat is only around 5-6 years. ",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+    {
+        "category": "Animals",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "Rabbits are rodents.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+    {
+        "category": "Animals", "type": "boolean", "difficulty": "easy",
+        "question": "A flock of crows is known as a homicide.", "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+    {
+        "category": "Animals", "type": "boolean", "difficulty": "medium", "question": "The Platypus is a mammal.",
+        "correct_answer": "True", "incorrect_answers": ["False"]
+    },
+    {
+        "category": "Animals", "type": "boolean", "difficulty": "easy",
+        "question": "The Killer Whale is considered a type of dolphin.", "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "category": "Animals", "type": "boolean", "difficulty": "easy", "question": "Rabbits are carnivores.",
+        "correct_answer": "False", "incorrect_answers": ["True"]
+    },
+    {
+        "category": "Animals", "type": "boolean", "difficulty": "easy",
+        "question": "The internet browser Firefox is named after the Red Panda.", "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "category": "Mythology", "type": "boolean", "difficulty": "easy",
+        "question": "In Norse mythology, Thor once dressed as a woman.", "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "category": "Mythology",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "According to Greek Mythology, Zeus can control lightning.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "category": "Mythology", "type": "boolean", "difficulty": "medium",
+        "question": "According to Norse mythology, Loki is a mother.", "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {"category": "Mythology", "type": "boolean", "difficulty": "medium",
+     "question": "The Japanese god Izanagi successfully returned his wife Izanami from the Underworld.",
+     "correct_answer": "False", "incorrect_answers": ["True"]},
+    {
+        "category": "Mythology", "type": "boolean", "difficulty": "hard",
+        "question": "Rannamaari was a sea demon that haunted the people of the Maldives and had to be appeased monthly with the sacrifice of a virgin girl.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "category": "Mythology",
+        "type": "boolean", "difficulty": "medium",
+        "question": "In Greek mythology, Hera is the goddess of harvest.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+    {"category": "Mythology", "type": "boolean", "difficulty": "easy",
+     "question": "According to Greek Mythology, Atlas was an Olympian God.",
+     "correct_answer": "False", "incorrect_answers": ["True"]},
+    {
+        "category": "Mythology", "type": "boolean", "difficulty": "medium",
+        "question": "The Roman god &quot;Jupiter&quot; was first known as &quot;Zeus&quot; to the Greeks.",
+        "correct_answer": "True", "incorrect_answers": ["False"]
+    },
+    {
+        "category": "Mythology", "type": "boolean", "difficulty": "easy",
+        "question": "A wyvern is the same as a dragon.",
+        "correct_answer": "False", "incorrect_answers": ["True"]
+    },
+    {
+        "category": "Mythology",
+        "type": "boolean",
+        "difficulty": "hard",
+        "question": "Janus was the Roman god of doorways and passageways.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "category": "Geography",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "Seoul is the capital of North Korea.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+    {
+        "category": "Geography",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "There are no roads in\/out of Juneau, Alaska.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "category": "Geography",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "San Marino is the only country completely surrounded by another country.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+    {
+        "category": "Geography",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "The Southeast Asian island of Borneo is politically divided among 3 countries.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "category": "Geography",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "There exists an island named &quot;Java&quot;.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "category": "Geography",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "There are no deserts in Europe.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "category": "Geography",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "The Sonoran Desert is located in eastern Africa.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+    {
+        "category": "Geography",
+        "type": "boolean", "difficulty": "easy",
+        "question": "St. Louis is the capital of the US State Missouri.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+    {
+        "category": "Geography",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "You could walk from Norway to North Korea while only passing through Russia.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "category": "Geography",
+        "type": "boolean",
+        "difficulty": "hard",
+        "question": "Only one country in the world starts with the letter Q.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    }
+]
